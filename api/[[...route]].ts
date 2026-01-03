39a7aa1bcb82c66a4aa8fd9a4b00ec1095c7e8f4
keisuke0322 <130468981+keisuke0322@users.noreply.github.com>
2026-01-03 12:15:59 +0900

    New feature (#18)
    * fix: change default export to named export for handler in [[...route]].ts
    
    * fix: build types package to JS for Vercel serverless functions
    
    * fix: use default export for Vercel Edge Function handler
    
    * fix: remove edge runtime config to use Node.js runtime instead
    
    ---------
    
    Co-authored-by: keisuke <keuske0322@gmail.com>

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -1,8 +1,4 @@
 import { handle } from "hono/vercel";
 import { app } from "../apps/api/src/index.js";
 
-export const config = {
-  runtime: "edge",
-};
-
-export const handler = handle(app);
+export default handle(app);
